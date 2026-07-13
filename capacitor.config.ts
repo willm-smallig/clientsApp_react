b1b7f869824f87e262fb9fbc92b5396ad4fb4394
willm smalling <wellintonbg@mail.com>
2026-07-13 09:19:29 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clientsApp_React',
+  webDir: 'dist'
+};
+
+export default config;
